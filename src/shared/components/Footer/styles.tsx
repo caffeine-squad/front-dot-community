d8254d09320e4b40094890353e0fc534344f72d6
Andreza <andrezaleal@alu.ufc.br>
2022-11-20 21:50:49 -0300

    :lipstick: update footer

--- src/shared/components/Footer/styles.tsx
+++ src/shared/components/Footer/styles.tsx
@@ -3,20 +3,21 @@ import styled from "styled-components";
 export const Container = styled.footer`
 display: flex;
 justify-content: center;
+flex-direction: column;
 align-items: center;
 width:100%;
 bottom:0;
-padding: 1.25rem 1.563rem;
+padding: 0.94rem 0;
 background: #62D2A2;
-
-h1{
-  font-weight: 600;
-  color: #000;
-  font-size: 1rem;
-  cursor: pointer;
-}
 
 @media (max-weight:650px) {
   height: 60px;
 }
+`
+export const SectionIcon = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width:100%;
+padding: 0.8rem 0;
 `
