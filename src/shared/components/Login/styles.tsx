f5f10fd42abf39634d75f1e747d85c09001df551
Andreza <andrezaleal@alu.ufc.br>
2022-11-20 21:43:51 -0300

    :recycle: removing unnecessary code

--- src/shared/components/Login/styles.tsx
+++ src/shared/components/Login/styles.tsx
@@ -19,6 +19,7 @@ export const LoginSection = styled.section`
     flex-direction: row;
     justify-content: center;
   }
+  
 `;
 
 export const Line = styled.div`
@@ -27,3 +28,37 @@ border-radius: 0.125rem;
 padding: 1rem 0 2rem 1rem;
 margin: 2rem 0 4rem 0;
 `;
+
+export const ButtonLink = styled.button`
+color: #29A073;
+margin-left: 0.3rem;
+
+`
+export const DropDownContent = styled.div`
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+
+  a{
+    color: black;
+    text-decoration: none;
+    display: block;
+    padding: 12px 16px;
+    &:hover{
+      background-color: #62D2A2;
+      color:black;
+    }
+  }
+`
+export const Dropdown = styled.div`
+  position: relative;
+  display: inline-block;
+    
+  &:hover ${DropDownContent}{
+    display:block;
+  }
+
+`
